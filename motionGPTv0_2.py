828501b788834b970b518aa12e51e89a0979713b
Sentexi <communicationbel@gmail.com>
2023-06-24 17:38:00 +0200

    Initial prompting functionality

---
+++ motionGPTv0_2.py
@@ -0,0 +1,22 @@
+
+def create_prompt():
+    prompt = """I want you to act as a debating theme generator. Debating is a competitive rhetorical format where debaters show their rhetorical and logical skills. 
+Here is a brief overview of the format we will be using: 
+
+There are two teams, a government and an opposition. The first speaker of the government introduces the topic and brings out an important argument for the team. The first speaker of the opposition criticises the motion brought forward by the government and presents arguments against it. The second speaker of each team makes a broader analysis of the topic bringing forward more arguments. Before the third speaker of each team speaks there are three independent speakers bringing out one argument in favour of either the government or the opposition. Finally the third speaker of each side summarises the debate, identifies clashes and explains why his side wins. The third speaker of each side does not introduce new arguments. The speaking time for each speaker is seven minutes and three and a half minutes for independent speakers. \
+
+The requirements for a good motion are thus the following: 
+
+1. The topic should have a clear claim to change something.
+2. The topic can be of political, philosophical or historical nature
+3. The arguments for the topic should be balanced, meaning that each side should have multiple arguments for them to bring forward.
+4. Different stakeholders should be involved that are affected by the changes introduced through the motion
+5. Smaller stakeholders or arguments should leave room for independent speakers to bring forward points.
+6. The topic should be new and innovative and not from the repertoire of usual debating motions.
+7. While the topic should not be too narrow, it should also be specific enough to avoid vagueness and foster focused debates.
+8. In addition to being new and innovative, the topic should avoid being a reworded version of a common or previously used topic.
+9. The topic should be phrased in clear, simple language that is easy to understand to avoid confusion or misinterpretation.
+10. The topic should avoid arguments that are heavily money focused, e.g. the success of the motion is dependant on money available.
+
+Based on these rules I want you to suggest five different debating motions."""
+    return prompt
---
+++ prompt.py
@@ -0,0 +1,32 @@
+import openai
+import motionGPTv0_2
+
+# Set up your OpenAI API credentials
+# This key will be deactivated on Github push, don't even try it
+openai.api_key = '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'
+
+v0_2prompt = motionGPTv0_2.create_prompt()
+
+# Define your prompt
+prompt = v0_2prompt
+initial_message = [{"role": "user", "content": prompt}]
+
+# Send the message to the API
+def send_message(messages)
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+      #max_tokens=100,
+      temperature=0.6,
+    )
+
+def print_response(response)
+    print(response)
+
+    # Extract the AI's response from the API response
+    ai_response = response.choices[0].message.content
+
+    print(ai_response)
+    
+
+
